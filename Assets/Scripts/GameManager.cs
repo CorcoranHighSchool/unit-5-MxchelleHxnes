9e004f64c20b1debb147b387be6ee4e6ea92f488
CHSGaming2 <144858803+CHSGaming2@users.noreply.github.com>
2024-02-23 13:16:35 -0800

    5.2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
     private float spawnRate = 1.0f;
     public List<GameObject> targets;
+    private int score;
+    public TextMeshProUGUI scoreText;
+
+    private void Awake()
+    {
+        instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget()); 
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        scoreText.text = " Score: " + score;
     }
 
-   IEnumerator SpawnTarget()
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    IEnumerator SpawnTarget()
     {
         while (true)
         {
@@ -20,6 +37,7 @@ public class GameManager : MonoBehaviour
             //spawn a target
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            
         }
     }
 }
